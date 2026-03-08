12c088329c9e8ef77cb6cc67171e400b0642545b
新元noname <13257656+xinyuanwm@user.noreply.gitee.com>
2026-03-08 18:45:20 +0800

    refactor(storage): 移除未使用的数据库实例
    移除了MessageStorageData类中未使用的全局数据库实例变量，
    该变量不再被代码逻辑引用，清理冗余代码以提高可维护性。

--- lib/storage/message_storage.dart
+++ lib/storage/message_storage.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shine/database/database.dart';
 
-final db = DatabaseProvider.instance;
 
 class MessageStorageData {
   final int id;
