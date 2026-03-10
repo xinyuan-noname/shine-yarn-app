db434598f049a28493d8827f2f3d36b350005e70
新元noname <13257656+xinyuanwm@user.noreply.gitee.com>
2026-03-11 07:55:10 +0800

    ``` feat(schedule): 添加课程表页面基础布局
    添加了shine主题导入并实现了课程表视图的基础UI结构，
    包括容器边距、圆角边框、阴影效果和线性渐变背景。
    ```

--- lib/views/schedule_view.dart
+++ lib/views/schedule_view.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shine/theme.dart';
 
 class ScheduleView extends StatelessWidget {
   const ScheduleView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox.expand(
+      child: Container(
+        padding: bodyPadding,
+        child: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+            gradient: whiteLinearGradient,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
